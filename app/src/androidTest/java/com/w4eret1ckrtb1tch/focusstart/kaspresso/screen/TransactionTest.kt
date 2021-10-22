072ad45600681792ff2799f80913204d07192467
Chapaev Anton <virsus2009@gmail.com>
2021-10-22 18:42:15 +0700

    added kaspresso UI test
    * for main screen - test case #1

--- app/src/androidTest/java/com/w4eret1ckrtb1tch/focusstart/kaspresso/screen/TransactionTest.kt
+++ app/src/androidTest/java/com/w4eret1ckrtb1tch/focusstart/kaspresso/screen/TransactionTest.kt
@@ -37,6 +37,7 @@ class TransactionTest : KTestCase() {
             step("Check data update button") {
                 MainScreen {
                     updateButton {
+                        isEnabled()
                         isDisplayed()
                         click()
                     }
@@ -48,37 +49,37 @@ class TransactionTest : KTestCase() {
                     Transaction(
                         charCode = "AUD",
                         name = "Австралийский доллар",
-                        value = 53.1292.toString(),
-                        rate = (-0.1198).toString(),
+                        value = BigDecimal(53.1292),
+                        rate = BigDecimal(-0.1198),
                         arrowId = R.drawable.ic_down
-                    ),
-                    Transaction(
-                        charCode = "AZN",
-                        name = "Азербайджанский манат",
-                        value = 41.7836.toString(),
-                        rate = (-0.0384).toString(),
-                        arrowId = R.drawable.ic_down
-                    ),
-                    Transaction(
-                        charCode = "GBP",
-                        name = "Фунт стерлингов Соединенного королевства",
-                        value = 97.9668.toString(),
-                        rate = 0.0168.toString(),
-                        arrowId = R.drawable.ic_up
                     ),
                     Transaction(
                         charCode = "AMD",
                         name = "Армянских драмов",
-                        value = 14.864.toString(),
-                        rate = (-0.0167).toString(),
+                        value = BigDecimal(14.864),
+                        rate = BigDecimal(-0.0167),
                         arrowId = R.drawable.ic_down
 
                     ),
                     Transaction(
+                        charCode = "GBP",
+                        name = "Фунт стерлингов Соединенного королевства",
+                        value = BigDecimal(97.9668),
+                        rate = BigDecimal(0.0168),
+                        arrowId = R.drawable.ic_up
+                    ),
+                    Transaction(
+                        charCode = "AZN",
+                        name = "Азербайджанский манат",
+                        value = BigDecimal(41.7836),
+                        rate = BigDecimal(-0.0384),
+                        arrowId = R.drawable.ic_down
+                    ),
+                    Transaction(
                         charCode = "BYN",
                         name = "Белорусский рубль",
-                        value = 29.1685.toString(),
-                        rate = 0.0355.toString(),
+                        value = BigDecimal(29.1685),
+                        rate = BigDecimal(0.0355),
                         arrowId = R.drawable.ic_up
                     )
                 )
@@ -90,8 +91,8 @@ class TransactionTest : KTestCase() {
     data class Transaction(
         val charCode: String,
         val name: String,
-        val value: String,
-        val rate: String,
+        val value: BigDecimal,
+        val rate: BigDecimal,
         @DrawableRes
         val arrowId: Int
     )
@@ -113,17 +114,17 @@ class TransactionTest : KTestCase() {
 
                         rate {
                             isDisplayed()
-                            hasText(transaction.rate)
+                            hasText(transaction.rate.toRate())
                         }
 
                         value {
                             isDisplayed()
-                            hasText(transaction.value)
+                            hasText(transaction.value.toValue())
                         }
 
                         arrowId {
                             isDisplayed()
-                            if (transaction.rate.toBigDecimal() >= BigDecimal(0)) hasDrawable(R.drawable.ic_up) else
+                            if (transaction.rate >= BigDecimal(0)) hasDrawable(R.drawable.ic_up) else
                                 hasDrawable(R.drawable.ic_down)
                         }
                     }
@@ -131,5 +132,11 @@ class TransactionTest : KTestCase() {
             }
         }
     }
+
+    private fun BigDecimal.toRate() =
+        String.format("( %1$.4f ₽ )", this)
+
+    private fun BigDecimal.toValue() =
+        String.format("%1$.4f ₽", this)
 
 }
